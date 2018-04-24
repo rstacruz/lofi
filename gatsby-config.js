fe95d054d03d54a39a9f440a151ef6f5959f7048
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-04-24 18:27:20 +0800

    Remove unused files
    This deletes some files like gatsby-*.js, and simplifies the home page to just
    "hello world."

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Lofi',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/header'
 import './index.css'
 
 const Layout = ({ children, data }) => (
@@ -10,21 +9,11 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: '' },
+        { name: 'keywords', content: '' },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    {children()}
   </div>
 )
 
