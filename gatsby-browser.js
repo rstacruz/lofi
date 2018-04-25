5419b071b72404b213dd99d417ca73d10aa708c3
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-04-25 14:24:50 +0800

    Attribute the gatsby mods properly
    The way unstated is hooked onto Gatsby was taken from their Redux
    examples. This adds links to the jsdoc blocks for those functions.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,8 @@ import { Router } from 'react-router-dom'
 import { Provider } from 'unstated'
 
 /**
+ * Hook unstated's `<Provider>` into the React tree.
+ *
  * See: https://github.com/gatsbyjs/gatsby/blob/master/examples/using-redux/gatsby-browser.js
  */
 
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { Provider } from 'unstated'
 import { renderToString } from 'react-dom/server'
 
+/**
+ * Hook unstated's `<Provider>` into the React tree.
+ *
+ * See: https://github.com/gatsbyjs/gatsby/blob/master/examples/using-redux/gatsby-ssr.js
+ */
+
 exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
   const ConnectedBody = () => <Provider>{bodyComponent}</Provider>
   replaceBodyHTMLString(renderToString(<ConnectedBody />))
