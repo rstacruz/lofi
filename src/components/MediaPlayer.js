06eb307e1038b4c8ffd5075e4b70cdff48faed95
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-04-25 15:33:17 +0800

    Move Soundcloud player component into its own file

---
+++ src/components/MediaPlayer.js
@@ -0,0 +1,160 @@
+import React from 'react'
+import qs from 'qs'
+import { Subscribe } from 'unstated'
+import getSoundcloud from '../../lib/soundcloud'
+import SoundcloudStore from '../stores/SoundcloudStore'
+
+/**
+ * Soundcloud player iframe.
+ *
+ * See: https://developers.soundcloud.com/docs/api/html5-widget
+ */
+
+export class MediaPlayer extends React.Component {
+  state = {}
+
+  render() {
+    const { SC } = this.state
+    const { dispatch } = this.props
+
+    const options = {
+      url: 'https://api.soundcloud.com/playlists/246258956',
+      auto_play: false,
+      buying: false,
+      liking: false,
+      download: false,
+      sharing: false,
+      show_artwork: true,
+      show_comments: false,
+      show_playcount: false,
+      show_teaser: true,
+      show_user: false,
+      hide_related: false
+      // visual: false,
+      // callback: true
+    }
+
+    const url = `https://w.soundcloud.com/player/?${qs.stringify(options)}`
+
+    const iframe = (
+      <iframe
+        ref={this.refIframe}
+        allow="autoplay"
+        width="100%"
+        height="465"
+        scrolling="no"
+        frameBorder="no"
+        src={url}
+      />
+    )
+
+    if (!SC) return null
+    return iframe
+  }
+
+  componentDidMount() {
+    getSoundcloud().then(SC => {
+      this.setState({ SC })
+    })
+  }
+
+  /**
+   * Passes down `actions` from the store to the SoundCloud widget API.
+   */
+
+  UNSAFE_componentWillReceiveProps(next) {
+    const prev = this.props
+
+    const actions = next.actions
+    const prevActions = prev.actions
+
+    if (actions !== prevActions && actions.length) {
+      actions.map(action => {
+        if (action.type === 'PLAY') {
+          this.state.widget.play()
+        } else if (action.type === 'PAUSE') {
+          this.state.widget.pause()
+        }
+      })
+    }
+  }
+
+  /**
+   * Initializes the `<iframe />`.
+   */
+
+  refIframe = el => {
+    this.iframe = el
+
+    const { widget, SC } = this.getAPI()
+    const { dispatch } = this.props
+
+    // Save widget to state
+    this.setState({ widget })
+
+    this.bindWidgetEvents()
+  }
+
+  /**
+   * Initializes the Soundcloud widget. Hooks Soundcloud events to `dispatch`.
+   */
+
+  bindWidgetEvents = el => {
+    widget.bind(SC.Widget.Events.READY, () => {
+      dispatch.setPlayerState('READY')
+
+      widget.getSounds(sounds => {
+        dispatch.setSounds(sounds)
+
+        // Skip to random track on startup
+        const idx = Math.round(sounds.length * Math.random())
+        widget.skip(idx)
+      })
+
+      widget.bind(SC.Widget.Events.PLAY, () => {
+        dispatch.setPlayerState('PLAYING')
+
+        widget.getCurrentSound(sound => {
+          dispatch.setSound(sound)
+        })
+      })
+
+      widget.bind(SC.Widget.Events.PAUSE, () => {
+        dispatch.setPlayerState('PAUSED')
+      })
+
+      widget.bind(SC.Widget.Events.FINISH, () => {
+        dispatch.setPlayerState('FINISH')
+      })
+    })
+  }
+
+  /**
+   * Returns the `widget` and `SC` objects.
+   */
+
+  getAPI = () => {
+    const { iframe } = this
+    if (!iframe) return {}
+
+    const { SC } = this.state
+    if (!SC) return {}
+
+    const widget = SC.Widget(iframe)
+    return { widget, SC }
+  }
+}
+
+/**
+ * Place this anywhere in your DOM tree. It listens to Soundcloud store.
+ */
+
+export default () => {
+  return (
+    <Subscribe to={[SoundcloudStore]}>
+      {soundcloud => (
+        <MediaPlayer dispatch={soundcloud} actions={soundcloud.state.actions} />
+      )}
+    </Subscribe>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,142 +1,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import qs from 'qs'
-import getSoundcloud from '../../lib/soundcloud'
 import { Subscribe } from 'unstated'
 import SoundcloudStore from '../stores/SoundcloudStore'
+import MediaPlayer from '../components/MediaPlayer'
 
 /**
- * Soundcloud player thing
- *
- * See: https://developers.soundcloud.com/docs/api/html5-widget
+ * Home page
  */
-
-export class MediaPlayer extends React.Component {
-  state = {}
-
-  render() {
-    const { SC } = this.state
-    const { store } = this.props
-
-    const options = {
-      url: 'https://api.soundcloud.com/playlists/246258956',
-      auto_play: false,
-      buying: false,
-      liking: false,
-      download: false,
-      sharing: false,
-      show_artwork: true,
-      show_comments: false,
-      show_playcount: false,
-      show_teaser: true,
-      show_user: false,
-      hide_related: false
-      // visual: false,
-      // callback: true
-    }
-
-    const url = `https://w.soundcloud.com/player/?${qs.stringify(options)}`
-
-    const iframe = (
-      <iframe
-        ref={this.refIframe}
-        allow="autoplay"
-        width="100%"
-        height="465"
-        scrolling="no"
-        frameBorder="no"
-        src={url}
-      />
-    )
-
-    if (!SC) return null
-    return iframe
-  }
-
-  componentDidMount() {
-    getSoundcloud().then(SC => {
-      this.setState({ SC })
-    })
-  }
-
-  /**
-   * Passes down `actions` from the store to the SoundCloud widget API.
-   */
-
-  UNSAFE_componentWillReceiveProps(next) {
-    const prev = this.props
-
-    const actions = next.actions
-    const prevActions = prev.actions
-
-    if (actions !== prevActions && actions.length) {
-      actions.map(action => {
-        if (action.type === 'PLAY') {
-          this.state.widget.play()
-        } else if (action.type === 'PAUSE') {
-          this.state.widget.pause()
-        }
-      })
-    }
-  }
-
-  /**
-   * Initializes the `<iframe />`.
-   */
-
-  refIframe = el => {
-    this.iframe = el
-
-    const { widget, SC } = this.getAPI()
-    const { store } = this.props
-
-    // Save widget to state
-    this.setState({ widget })
-
-    widget.bind(SC.Widget.Events.READY, () => {
-      store.setPlayerState('READY')
-
-      widget.getSounds(sounds => {
-        store.setSounds(sounds)
-        // Skip to random track
-        const idx = Math.round(sounds.length * Math.random())
-        widget.skip(idx)
-      })
-
-      widget.bind(SC.Widget.Events.PLAY, () => {
-        store.setPlayerState('PLAYING')
-
-        widget.getCurrentSound(sound => {
-          store.setSound(sound)
-        })
-      })
-
-      widget.bind(SC.Widget.Events.PAUSE, () => {
-        store.setPlayerState('PAUSED')
-      })
-
-      widget.bind(SC.Widget.Events.FINISH, () => {
-        // Is this right?
-        store.setPlayerState('FINISH')
-      })
-    })
-  }
-
-  /**
-   * Returns the `widget` and `SC` objects.
-   */
-
-  getAPI = () => {
-    const { iframe } = this
-    if (!iframe) return {}
-
-    const { SC } = this.state
-    if (!SC) return {}
-
-    const widget = SC.Widget(iframe)
-    return { widget, SC }
-  }
-}
 
 export const IndexPage = () => (
   <Subscribe to={[SoundcloudStore]}>
@@ -167,7 +37,7 @@ export const IndexPage = () => (
         ) : null}
 
         <div className="soundcloud-area">
-          <MediaPlayer store={soundcloud} actions={soundcloud.state.actions} />
+          <MediaPlayer />
         </div>
       </div>
     )}
