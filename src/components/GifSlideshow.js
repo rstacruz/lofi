eac5757cf6915db68e650ceffa2ed7d2c6323fb7
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-04-25 19:14:26 +0800

    Add styled-jsx for Slideshow

--- src/components/GifSlideshow.js
+++ src/components/GifSlideshow.js
@@ -1,3 +1,22 @@
 import React from 'react'
+import * as VARS from '../styles/variables'
 
-export default () => <div className="GifSlideshow" />
+export default () => (
+  <div className="GifSlideshow">
+    <style jsx>{`
+      .GifSlideshow {
+        width: 100%;
+        height: 100%;
+        background: ${VARS.gridBg};
+        border: solid 6px ${VARS.gridBg};
+        background:
+          linear-gradient(135deg, transparent 1px, ${
+            VARS.gridBg
+          } 2px) left top / ${VARS.gridSize} ${VARS.gridSize},
+          url('${VARS.gridImage}') center center / cover no-repeat, ${
+      VARS.gridBg
+    };
+      }
+    `}</style>
+  </div>
+)
