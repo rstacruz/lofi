305604843ed8a48bb4f2e527a3525d20aa849042
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-04-27 20:02:19 +0800

    Really fix home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,16 @@
 import React from 'react'
 import IndexPage from '../components/IndexPage'
 
+/**
+ * Template for the home page.
+ */
+
 export const IndexTemplate = props => {
   // Happened on a `gatsby build`, no idea why though!
-  if (!props.pageResources) {
-    console.error('IndexTemplate: no pageResources found')
-    return <div />
-  }
+  console.log(JSON.stringify(props, null, 2))
 
-  const edges = props.pageResources.json.data.allSitePage.edges
-
-  const pages = edges
-    .filter(edge => edge.node.context.title)
-    .map(edge => ({ path: edge.node.path, ...edge.node.context }))
+  const edges = props.data.allSitePage.edges
+  const pages = getPageNodes(edges)
 
   return (
     <div>
@@ -23,7 +21,33 @@ export const IndexTemplate = props => {
   )
 }
 
+/**
+ * Returns pages.
+ *
+ * A page has `{ path, title, href, soundcloudURL }`.
+ */
+
+function getPageNodes (edges) {
+  return edges
+    .filter(
+      edge =>
+        edge.node &&
+        edge.node.context &&
+        edge.node.path &&
+        edge.node.context.title
+    )
+    .map(edge => ({ path: edge.node.path, ...edge.node.context }))
+}
+
+/*
+ * Export
+ */
+
 export default IndexTemplate
+
+/*
+ * GraphQL query to pull all stations.
+ */
 
 export const pageQuery = graphql`
   query IndexPageQuery {
