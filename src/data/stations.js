583fefcafbc8a1ad501d5a5eca41cdc63ca871f9
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-04-28 10:20:36 +0800

    Annotate station genres

--- src/data/stations.js
+++ src/data/stations.js
@@ -5,11 +5,13 @@
 const STATIONS = {
   '/lofi-hiphop': {
     title: 'Lofi Beats',
+    genre: 'Hiphop',
     href: 'https://soundcloud.com/parzival6/sets/lo-fi-hip-hop',
     soundcloudURL: 'https://api.soundcloud.com/playlists/246258956'
   },
   '/timewriter': {
     title: 'Lazy Sundays',
+    genre: 'Tech House',
     href: 'https://soundcloud.com/jessinneijts/sets/timewriter-lazy-sundays',
     soundcloudURL: 'https://api.soundcloud.com/playlists/109759947'
   },
@@ -20,17 +22,20 @@ const STATIONS = {
   // },
   '/morning-bliss': {
     title: 'Morning Bliss',
+    genre: 'Indie',
     href:
       'https://soundcloud.com/user-876658341/sets/sunday-morning-kisses-and',
     soundcloudURL: 'https://api.soundcloud.com/playlists/424032182'
   },
   '/worldwide-fm': {
     title: 'Worldwide FM',
+    genre: 'Electronic',
     href: 'https://soundcloud.com/worldwide-fm/sets/new-music-worldwide-guest-mixes',
     soundcloudURL: 'https://api.soundcloud.com/playlists/331692615'
   },
   '/mojo-essentials': {
     title: 'Mojo Essentials',
+    genre: 'Soul/Jazz',
     href: 'https://soundcloud.com/mojo_club/sets/essentials',
     soundcloudURL: 'https://api.soundcloud.com/playlists/230735978'
   }
