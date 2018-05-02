895eb13aed6476e608dde05597eabe4c17048402
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-05-02 18:37:39 +0800

    Fix tickedoff flow error

---
+++ flow-typed/npm/tickedoff_vx.x.x.js
@@ -0,0 +1,6 @@
+// flow-typed signature: 1038b15dbf0c5fbe745ef9d17ef23037
+// flow-typed version: <<STUB>>/tickedoff_v1.0.2/flow_v0.71.0
+
+declare module 'tickedoff' {
+  declare module.exports: (() => void) => void
+}
